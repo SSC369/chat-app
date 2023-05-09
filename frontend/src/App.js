552a7a3c30441a6e8b3150e4368df9f04c2df445
SSC369 <sai9821c@gmail.com>
2023-05-09 12:43:28 +0530

    chat-app

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import SetAvatar from "./components/SetAvatar";
+import Chat from "./pages/Chat";
+
+//
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/setAvatar" element={<SetAvatar />} />
+      <Route path="/" element={<Chat />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;
